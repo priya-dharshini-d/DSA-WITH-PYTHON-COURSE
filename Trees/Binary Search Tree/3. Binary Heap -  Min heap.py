f55d00836a9cbaed23fee9a9ded13aad716f5826
PRIYA DHARSHINI D <priyadharshiniofficial2024@gmail.com>
2025-06-24 11:58:01 +0530

    Update 3. Binary Heap -  Min heap.py

--- Trees/Binary Search Tree/3. Binary Heap -  Min heap.py
+++ Trees/Binary Search Tree/3. Binary Heap -  Min heap.py
@@ -1,74 +1,63 @@
-class Heap:
+class MinHeap:
     def __init__(self):
-        self.heapList = [0]  # Index 0 is dummy
+        self.heap = [0]  # Dummy at index 0
         self.size = 0
 
-    def parent(self, index):
+    def get_parent_index(self, index):
         return index // 2
 
-    def left_child(self, index):
+    def get_left_child_index(self, index):
         return 2 * index
 
-    def right_child(self, index):
+    def get_right_child_index(self, index):
         return 2 * index + 1
 
     def get_min(self):
-        if self.size == 0:
-            return None
-        return self.heapList[1]
+        return None if self.size == 0 else self.heap[1]
 
-    def percolate_up(self, i):
-        while i // 2 > 0:
-            if self.heapList[i] < self.heapList[i // 2]:  # < for min-heap
-                self.heapList[i], self.heapList[i // 2] = self.heapList[i // 2], self.heapList[i]
-            i = i // 2
+    def bubble_up(self, index):
+        while index > 1:
+            parent = self.get_parent_index(index)
+            if self.heap[index] < self.heap[parent]:  # < for MinHeap
+                self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+            index = parent
 
-    def insert(self, item):
-        self.heapList.append(item)
+    def insert(self, value):
+        self.heap.append(value)
         self.size += 1
-        self.percolate_up(self.size)
+        self.bubble_up(self.size)
 
-    def percolate_down(self, i):
-        while (i * 2) <= self.size:
-            min_child = self.min_child(i)
-            if self.heapList[i] > self.heapList[min_child]:  # > for min-heap
-                self.heapList[i], self.heapList[min_child] = self.heapList[min_child], self.heapList[i]
-            i = min_child
+    def bubble_down(self, index):
+        while index * 2 <= self.size:
+            smaller_child = self.get_smaller_child_index(index)
+            if self.heap[index] > self.heap[smaller_child]:  # > for MinHeap
+                self.heap[index], self.heap[smaller_child] = self.heap[smaller_child], self.heap[index]
+            index = smaller_child
 
-    def min_child(self, i):
-        if i * 2 + 1 > self.size:
-            return i * 2
+    def get_smaller_child_index(self, index):
+        left = self.get_left_child_index(index)
+        right = self.get_right_child_index(index)
+        if right > self.size:
+            return left
+        
+        if self.heap[left] < self.heap[right]:  # < for MinHeap
+            return left 
         else:
-            if self.heapList[i * 2] < self.heapList[i * 2 + 1]:  # < for min-heap
-                return i * 2
-            else:
-                return i * 2 + 1
+            return right
 
     def delete_min(self):
         if self.size == 0:
             return None
-        retval = self.heapList[1]
-        self.heapList[1] = self.heapList[self.size]
+        min_value = self.heap[1]
+        self.heap[1] = self.heap[self.size]
+        self.heap.pop()
         self.size -= 1
-        self.heapList.pop()
-        self.percolate_down(1)
-        return retval
+        
+        self.bubble_down(1)
+        return min_value
 
-    def build_heap_from_array(self, A):                #O(n) build-heap is faster than inserting elements one-by-one (O(n log n)).
-        i = len(A) // 2
-        self.size = len(A)
-        self.heapList = [0] + A[:]
-        while i > 0:
-            self.percolate_down(i)
-            i -= 1
-h = Heap()
-h.build_heap_from_array([9, 5, 6, 2, 3])
-print("Min Heap array:", h.heapList[1:])  # Ignore dummy at index 0
-
-h.insert(1)
-print("After insert(1):", h.heapList[1:])
-
-print("Min element (peek):", h.get_min())
-
-print("Deleted min:", h.delete_min())
-print("After delete_min():", h.heapList[1:])
+    def build_from_list(self, nums):
+        self.size = len(nums)
+        self.heap = [0] + nums[:]
+        for i in range(self.size // 2, 0, -1):
+            self.bubble_down(i)
